a2cf10528c15f8dd87e4b3d686db662071b79b0d
haru <haru@haru52.com>
2022-07-19 15:10:31 +0900

    chore: update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,29 @@
 cd "$(dirname "$0")/../src" || exit 1
 
 src_path=$(pwd)
-mkdir -p ~/bin
+bin_path=$HOME/bin
+mkdir -p "$bin_path"
+ln -sfv "$src_path"/base_template_cli.py "$bin_path"/base-template-cli
 
-ln -sfv "$src_path"/base_template_cli.py ~/bin/base-template-cli
+if [ "$PATH" = "$bin_path" ] || [ "$PATH" = "${bin_path}/" ]; then
+  exit 0
+fi
+
+if echo "$PATH" | grep -q -e "${bin_path}:" -e "${bin_path}/:" -e ":${bin_path}"; then
+  exit 0
+fi
+
+shell_name=$(basename "$SHELL")
+
+case $shell_name in
+  bash) shell_rc_path=$HOME/.bashrc ;;
+  zsh) shell_rc_path=$HOME/.zshrc ;;
+  *)
+    printf "Run the following command:\n\n"
+    # shellcheck disable=SC2016
+    echo 'echo "export PATH=\"\${HOME}/bin:\${PATH}\"" >> <your shell rc file path (e.g., ~/.bashrc)>'
+    exit 0
+    ;;
+esac
+
+echo "export PATH=\"\${HOME}/bin:\${PATH}\"" >> "$shell_rc_path"
