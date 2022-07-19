a4ac5d3d499fc16fb469c52fe5d663ebb4571c89
haru <haru@haru52.com>
2022-07-19 14:46:32 +0900

    chore: add `--version` option

--- src/base_template_cli.py
+++ src/base_template_cli.py
@@ -3,8 +3,10 @@
 import click
 from applyer import Applyer
 from di_manager import DIManager
+import version_getter
 
 @click.command()
+@click.version_option(version=version_getter.VersionGetter.get_version(), prog_name='Base Template CLI')
 @click.argument('destination_path')
 @click.argument('base_template_path')
 def apply(destination_path, base_template_path):
